0b8feffa819dd89f5c9682168d32d0d78b02cf69
windows <windows@microsoft.com>
2022-11-08 12:33:39 +0530

    install boostrap and return

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-    <h1>welcome to react.js</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
+    <h1 className="border mt-5">welcome to react.js</h1>
+    <button type="button" className="btn btn-primary mb-5">Primary</button>
+    <button type="button" className="btn btn-secondary ms-5">Secondary</button>
+<button type="button" className="btn btn-success m-s-5">Success</button>
+<button type="button" className="btn btn-danger ml-5">Danger</button>
+<button type="button" className="btn btn-warning">Warning</button>
+<button type="button" className="btn btn-info">Info</button>
+<button type="button" className="btn btn-light">Light</button>
+<button type="button" className="btn btn-dark">Dark</button>
     </div>
     )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from "react";
+
+function Myhome()
+{
+    // return <h1>web</h1>
+
+    // return(
+    //     <h1>welcome to react</h1>
+    // )
+
+
+    // return(
+    //     <div>
+    //     <h1>welcome to react</h1>
+    //     <h1>welcome to react</h1>
+    //     <h1>welcome to react</h1>
+    //     </div>
+    // )
+
+    // return(
+    //     <>
+    //     <h1>welcome to react</h1>
+    //     <h1>welcome to react</h1>
+    //     <h1>welcome to react</h1>
+    //     </>
+    // )
+
+    return(
+        <Fragment>
+        <h1>welcome to react</h1>
+        <h1>welcome to react</h1>
+        <h1>welcome to react</h1>
+        </Fragment>
+    )
+
+
+}
+
+export default Myhome
