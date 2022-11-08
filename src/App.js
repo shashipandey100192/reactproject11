e2e88e1863e2a6e7a5bf5e949b380b6aee37804b
lakhanvermalk.34 <lakhanvermalk.34@gmail.com>
2022-11-08 12:50:20 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     <h1>welcome to react.js</h1>
     <button type="button" class="btn btn-primary">Primary</button>
     <button type="button" className="btn btn-secondary mt-3">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
+<button type="button" className="btn btn-success">Success</button>
+<button type="button" className="btn btn-danger">Danger</button>
+<button type="button" className="btn btn-warning">Warning</button>
+<button type="button" className="btn btn-info">Info</button>
+<button type="button" className="btn btn-light">Light</button>
+<button type="button" className="btn btn-dark">Dark</button>
     </div>
     )
 }
