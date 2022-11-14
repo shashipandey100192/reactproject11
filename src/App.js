10c4ff07eeba6da48e642836245136b6f4a6f4de
windows <windows@microsoft.com>
2022-11-14 12:37:31 +0530

    add some change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Fragment } from 'react';
 
 function App() {
   return (
+    <Fragment>
     <div>
-    <h1 className="border mt-5">welcome to react.js</h1>
-    <button type="button" className="btn btn-primary mb-5">Primary</button>
-    <button type="button" className="btn btn-secondary ms-5">Secondary</button>
-<button type="button" className="btn btn-success m-s-5">Success</button>
-<button type="button" className="btn btn-danger ml-5">Danger</button>
-<button type="button" className="btn btn-warning">Warning</button>
-<button type="button" className="btn btn-info">Info</button>
-<button type="button" className="btn btn-light">Light</button>
-<button type="button" className="btn btn-dark">Dark</button>
+              <h1 className="border mt-5">welcome to react.js</h1>
+              <button type="button" className="btn btn-primary mb-5">Primary</button>
+              <button type="button" className="btn btn-secondary ms-5">Secondary</button>
+          <button type="button" className="btn btn-success m-s-5">Success</button>
+          <button type="button" className="btn btn-danger ml-5">Danger</button>
+          <button type="button" className="btn btn-warning">Warning</button>
+          <button type="button" className="btn btn-info">Info</button>
+          <button type="button" className="btn btn-light">Light</button>
+          <button type="button" className="btn btn-dark">Dark</button>
+          <img src="image/img2.jpg" alt="ducat" />
     </div>
+    <div>web</div>
+    </Fragment>
     )
 }
 
