7066d0babdfdddf31afd5606ad63735c2a750013
zeus3011 <adi.jadon7@gmail.com>
2022-11-14 12:49:55 +0530

    space added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,14 +7,15 @@ function App() {
   return (
     <div>
     <h1>welcome to react.js</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" className="btn btn-primary m-2">Primary</button>
+    <button type="button" className="btn btn-secondary m-2">Secondary</button>
+<button type="button" className="btn btn-success m-2">Success</button>
+<button type="button" className="btn btn-danger">Danger</button>
+<button type="button" className="btn btn-warning">Warning</button>
+<button type="button" className="btn btn-info">Info</button>
+<button type="button" className="btn btn-light">Light</button>
+
+<button type="button" className="btn btn-dark">Dark</button>
     </div>
     )
 }
