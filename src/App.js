d31abd8cec6e4a7e0e35d67c2ec9c8ba915d96c4
zeus3011 <adi.jadon7@gmail.com>
2022-11-14 12:55:47 +0530

    no change

--- src/App.js
+++ src/App.js
@@ -2,48 +2,20 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Fragment } from 'react';
 
 function App() {
   return (
-    <Fragment>
     <div>
-<<<<<<< HEAD
-              <h1 className="border mt-5">welcome to react.js</h1>
-              <button type="button" className="btn btn-primary mb-5">Primary</button>
-              <button type="button" className="btn btn-secondary ms-5">Secondary</button>
-          <button type="button" className="btn btn-success m-s-5">Success</button>
-          <button type="button" className="btn btn-danger ml-5">Danger</button>
-          <button type="button" className="btn btn-warning">Warning</button>
-          <button type="button" className="btn btn-info">Info</button>
-          <button type="button" className="btn btn-light">Light</button>
-          <button type="button" className="btn btn-dark">Dark</button>
-          <img src="image/img2.jpg" alt="ducat" />
-=======
     <h1>welcome to react.js</h1>
-<<<<<<< HEAD
     <button type="button" className="btn btn-primary m-2">Primary</button>
     <button type="button" className="btn btn-secondary m-2">Secondary</button>
 <button type="button" className="btn btn-success m-2">Success</button>
-=======
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" className="btn btn-secondary mt-3">Secondary</button>
-<button type="button" className="btn btn-success">Success</button>
->>>>>>> 89f3502f5f891a918160d2fa526323ffa746c684
 <button type="button" className="btn btn-danger">Danger</button>
 <button type="button" className="btn btn-warning">Warning</button>
 <button type="button" className="btn btn-info">Info</button>
 <button type="button" className="btn btn-light">Light</button>
-<<<<<<< HEAD
-
-<button type="button" className="btn btn-dark">Dark</button>
-=======
-<button type="button" className="btn btn-dark">Dark</button>
->>>>>>> 253b55af47c85098ecca8b03f87875876888493f
->>>>>>> 89f3502f5f891a918160d2fa526323ffa746c684
+<button type="button" className="btn btn-dark">ark</button>
     </div>
-    <div>web</div>
-    </Fragment>
     )
 }
 
