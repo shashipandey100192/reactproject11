118d52be2862d9aeb855e84ae9dec29416e25216
nitish770 <nitishkumarranjan100@gmail.com>
2022-11-15 13:39:30 +0530

    change2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <Fragment>
     <div>
-<<<<<<< HEAD
               <h1 className="border mt-5">welcome to react.js</h1>
               <button type="button" className="btn btn-primary mb-5">Primary</button>
               <button type="button" className="btn btn-secondary ms-5">Secondary</button>
@@ -19,20 +18,10 @@ function App() {
           <button type="button" className="btn btn-light">Light</button>
           <button type="button" className="btn btn-dark">Dark</button>
           <img src="image/img2.jpg" alt="ducat" />
-=======
-    <h1>welcome to react.js</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" className="btn btn-secondary mt-3">Secondary</button>
-<button type="button" className="btn btn-success">Success</button>
-<button type="button" className="btn btn-danger">Danger</button>
-<button type="button" className="btn btn-warning">Warning</button>
-<button type="button" className="btn btn-info">Info</button>
-<button type="button" className="btn btn-light">Light</button>
-<button type="button" className="btn btn-dark">Dark</button>
->>>>>>> 253b55af47c85098ecca8b03f87875876888493f
     </div>
+    </Fragment>
     )
->>>>>>> 0c6533d76aecae9b2c7c80c9402e0c4fe59ec78d
+
 }
 
-export default App;
+export default App
